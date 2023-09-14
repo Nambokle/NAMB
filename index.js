19b49dc57a5ffb2cb37371ec479ace4d72e7d03d
Nambokle <135355990+Nambokle@users.noreply.github.com>
2023-09-14 23:00:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js')
-const client = new Discord.Client({ intents: [
-  Discord.GatewayIntentBits.Guilds,
-  Discord.GatewayIntentBits.GuildMessages
-]})
+const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command')
 const fs = require("fs");
